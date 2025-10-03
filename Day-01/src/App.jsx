ec5fcf66e8660ec6b125fcdac0656a85eb3470c3
Dabhi Amit <amitdabhi526@gmail.com>
2025-10-03 12:24:51 +0530

    component

--- Day-01/src/App.jsx
+++ Day-01/src/App.jsx
@@ -2,14 +2,17 @@ import Jsx from "./Jsx";
 import State from "./State";
 import PrevState from "./prevState";
 import Props from "./Props";
+import { Component, FunctionalComponent } from "react";
 
 function App (){
     return(
         <>
             {/* <Jsx></Jsx> */}
-            <State></State>
+            {/* <State></State> */}
             {/* <PrevState></PrevState> */}
             <Props greeting={"Good morning"}></Props>
+            <Component></Component>
+            <FunctionalComponent></FunctionalComponent>
         </>
         
     )
