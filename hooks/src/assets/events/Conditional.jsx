f896ec11417de3c59608e06529bc0e9d4d8ca5fc
Dabhi Amit <amitdabhi526@gmail.com>
2025-10-10 13:54:55 +0530

    if else in react

--- hooks/src/assets/events/Conditional.jsx
+++ hooks/src/assets/events/Conditional.jsx
@@ -21,3 +21,34 @@
 
 // conditional example 2
 
+import React, { useState } from "react";
+
+const Conditional = () => {
+    const [color, setColor] = useState("green");
+
+    let content;
+
+    if (color === "green"){
+        content = (
+            <h1>You have selected green color</h1>
+        )
+    }
+    else if(color === "blue"){
+        content = (
+            <h1>You have selected blue color</h1>
+        )
+    }
+    else{
+        content = (
+            <h1> you are not allowed to select other color only you can select green or blue</h1>
+        )
+    }
+
+    return(
+        <>
+            {content}
+        </>
+    )
+}
+
+export default Conditional;
