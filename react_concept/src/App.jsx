b956b1c0aea93566b97206de2381d0dac13ba4a0
Dabhi Amit <amitdabhi526@gmail.com>
2025-11-17 12:30:23 +0530

    suspense and lazy loading

--- react_concept/src/App.jsx
+++ react_concept/src/App.jsx
@@ -1,68 +1,50 @@
-import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from './routes/MainLayout'; 
+import About from './component/About';
+import Service from './component/Service';
+import ErrorPage from './component/ErrorPage';
+import ModernLoader from './component/Loading';
+import { lazy, Suspense } from 'react';
 
-import UseState from "./useState/UseState";
-import ObjectUseState from "./useState/ObjectUseState"
-import LifeCycle from "./useState/LifeCycle"
-import UseEffect from "./useState/UseEffect";
-import OnClick from "./events/Onclick";
-import OnChange from "./events/Onchange";
-import OnSubmit from "./events/OnSubmit";
-import PassingArgument from "./events/PassingArgument";
-import Conditional from "./events/Conditional";
-import List from "./lists/List"
-import ObjectList from "./lists/ObjectList";
-import Focus from "./ref/Focus";
-import Controlled from "./forms/controlled";
-import Uncontrolled from "./forms/Uncontrolled";
-import WorkingWithForms from "./forms/WorkingWithForms";
-import Validation from "./forms/Validation";
-import FormikForm from "./forms/FormikForm";
+const Home = lazy(() => import('./component/Home'));
 
-const App = () => /* const [input, setInput] = useState("");*/ (
-  <>
-    {/* <UseState></UseState> */}
+const Product = lazy (() => import('./component/Product'));
 
-    {/* <ObjectUseState></ObjectUseState> */}
+const App = () => {
 
-    {/* <LifeCycle></LifeCycle> */}
+  const router = createBrowserRouter ([
+    {
+      path:"/",
+      element:<MainLayout />,
+      errorElement:<ErrorPage/>,
+      children: [
+        {
+          index:true,
+          element:<Home />
+        },
+        {
+          path:"about",
+          element:<About />
+        },
+        {
+          path:"service",
+          element:<Service />
+        },
+        {
+          path:"product/:id",
+          element:<Product />
+        },
+      ]
+    }
+  ])
 
-    {/* <button onClick={() => setShow((prev) => !prev)}>
-          {show ? "hide" : "show"}{" "}
-        </button> */}
-    {/* {show && <LifeCycle />} */}
-
-    {/* <UseEffect></UseEffect> */}
-
-    {/* <event></event> */}
-
-    {/* <OnClick></OnClick> */}
-
-    {/* <OnChange></OnChange> */}
-
-    {/* <><OnSubmit></OnSubmit></> */}
-
-    {/* passing argument in event using props  */}
-    {/* <input type="text" value={input} placeholder="Enter Name" onChange={(e) => setInput(e.target.value)}/>
-        <PassingArgument name={input}></PassingArgument> */}
-
-    {/* <Conditional></Conditional>*/}
-
-    {/* <List></List> */}
-
-    {/* <ObjectList></ObjectList> */}
-
-    {/* <Focus></Focus> */}
-
-    {/* <Controlled></Controlled> */}
-
-    {/* <Uncontrolled></Uncontrolled> */}
-
-    {/* <WorkingWithForms></WorkingWithForms> */}
-
-    {/* <Validation></Validation> */}
-
-    <FormikForm></FormikForm>
-  </>
-)
+  return (
+    <>
+      <Suspense fallback={<ModernLoader/>}>
+        <RouterProvider router={router}/>
+      </Suspense>
+    </>
+  )
+}
 
 export default App;
---
+++ react_concept/src/component/Product.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
+const Product = () => {
+
+    const { id } = useParams();
+
+    const navigate = useNavigate();
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      navigate("/");
+    }
+
+  return (
+    <div>
+      <h1>This is Product = { id }</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text"/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Product;
