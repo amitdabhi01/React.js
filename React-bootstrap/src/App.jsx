3c32c134739c4e60483f5d4442067b9f1dcff707
Dabhi Amit <amitdabhi526@gmail.com>
2025-11-20 13:57:35 +0530

    Add files via upload

--- React-bootstrap/src/App.jsx
+++ React-bootstrap/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import Navbar from './Component/Navbar';
+import Carousel from './Component/Carousel';
+  
 const App = () => {
   return (
     <>
-      
+      <header>
+        <Navbar/>
+        <Carousel/>
+      </header>
     </>
   )
 }
--- React-bootstrap/src/Component/Navbar.jsx
+++ React-bootstrap/src/Component/Navbar.jsx
@@ -1,6 +1,6 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function BasicExample() {
   return (
@@ -9,11 +9,11 @@ function BasicExample() {
         <Navbar.Brand href="#home">Travel</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="ms-auto text-center">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#service">Service</Nav.Link>
-            <Nav.Link href="#Contact">Contact</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#home">Service</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
