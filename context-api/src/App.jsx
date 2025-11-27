6cf8dcd450a4c453a05b00a84fdf069147875adf
Dabhi Amit <amitdabhi526@gmail.com>
2025-11-27 13:51:16 +0530

    create operation

---
+++ context-api/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExpenseForm from './components/ExpenseForm';
+
+const App = () => {
+  return (
+    <>
+      <ExpenseForm/>
+    </>
+  )
+}
+
+export default App;
