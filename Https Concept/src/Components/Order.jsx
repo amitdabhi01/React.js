a31e0d042c45c16590b760c9900b43b6d1bf3bfa
Dabhi Amit <amitdabhi526@gmail.com>
2025-12-23 14:09:58 +0530

    update

--- Https Concept/src/Components/Order.jsx
+++ Https Concept/src/Components/Order.jsx
@@ -17,7 +17,6 @@ const Order = ({ onHide, onShow }) => {
         setOrder(data);
 
         // console.log(data);
-        
       } catch (error) {
         console.log(error.message);
       }
@@ -25,6 +24,50 @@ const Order = ({ onHide, onShow }) => {
 
     handleOrderData();
   }, []);
+
+  // const handleOrderStatus = async (id, status) => {
+  //   try {
+  //     const res = await fetch(`http://localhost:5000/orders/${id}`, {
+  //       method: "PATCH",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ status }),
+  //     });
+
+  //     if (!res.ok) {
+  //       throw new Error("failed to update order status");
+  //     }
+
+  //     alert("order status updated successfully");
+
+  //     setOrders((prevOrder) =>
+  //       prevOrder.map((prod) =>
+  //         prod.id === id ? { ...prod, status: status } : prod
+  //       )
+  //     );
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
+
+  const handleOrderStatus = async (id, status) => {
+    try {
+      const res = await axios.patch(`http://localhost:5000/orders/${id}`, {
+        status,
+      });
+
+      setOrder((prevOrder) =>
+        prevOrder.map((prod) => {
+          prod.id === id ? { ...prod, status: status } : prod;
+        })
+      );
+
+      alert("order status updated successfully");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <>
@@ -41,46 +84,55 @@ const Order = ({ onHide, onShow }) => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            {order.length === 0 ? (<p>Your Order Cart is Empty</p>) :(
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Order</th>
-                            <th>Status</th>
-                            <th>Created At</th>
-                            <th>Total Amount</th>
-                            <th className="text-center" colSpan={2}>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {order.map((order) => (
-                            <tr key={order.id}>
-                                <td>{order.id}</td>
-                                <td>
-                                    {order.cart.map((item, index) => (
-                                        <ul key={index}>
-                                            <li>{item.name}</li>
-                                            <small>{item.quantity} ✖️ ₹{item.price}</small>
-                                        </ul>
-                                    ))}
-                                </td>
-                                <td>{order.status}</td>
-                                <td>
-                                    {new Date(order.createdAt).toLocaleString()}
-                                </td>
-                                <td>{order.totalAmount}</td>
-                                <td>
-                                    <Button variant="success">Update</Button>
-                                </td>
-                                <td>
-                                    <Button variant="danger">Delete</Button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            )}
+          {order.length === 0 ? (
+            <p>Your Order Cart is Empty</p>
+          ) : (
+            <Table>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Order</th>
+                  <th>Status</th>
+                  <th>Created At</th>
+                  <th>Total Amount</th>
+                  <th className="text-center" colSpan={2}>
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {order.map((ord) => (
+                  <tr key={ord.id}>
+                    <td>{ord.id}</td>
+                    <td>
+                      {ord.cart.map((item, index) => (
+                        <ul key={index}>
+                          <li>{item.name}</li>
+                          <small>
+                            {item.quantity} ✖️ ₹{item.price}
+                          </small>
+                        </ul>
+                      ))}
+                    </td>
+                    <td>{ord.status}</td>
+                    <td>{new Date(ord.createdAt).toLocaleString()}</td>
+                    <td>{ord.totalAmount}</td>
+                    <td>
+                      <Button
+                        variant="success"
+                        onClick={() => handleOrderStatus(ord.id, "completed")}
+                      >
+                        Complete
+                      </Button>
+                    </td>
+                    <td>
+                      <Button variant="danger">Delete</Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={onHide}>Close</Button>
