d8f0ff0f769d6c501867eab75de4824c53c044c7
Dabhi Amit <amitdabhi526@gmail.com>
2026-01-06 14:04:35 +0530

    Add files via upload

---
+++ Auth-FireBase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHODOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.envVITE_FIREBASE_APPID.
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
